4def913f93ae7b7e82468d6ba81a879965712796
jpineda5 <jpineda5@jhu.edu>
2022-04-10 23:46:34 -0400

    added post creation empty activity

---
+++ app/src/main/java/com/example/uimastyleit/PostDetails.java
@@ -0,0 +1,47 @@
+package com.example.uimastyleit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class PostDetails extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference dbRef;
+    private String userID;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        dbRef = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+        //getActionBar().setTitle("Post Details");
+    }
+
+
+
+
+}
