67492781451b53a46446ed4d199c9d92be19effe
Alexander Vargas <avargas7@jhu.edu>
2022-04-23 14:08:59 -0400

    updated item class

--- app/src/main/java/com/example/uimastyleit/Item.java
+++ app/src/main/java/com/example/uimastyleit/Item.java
@@ -3,21 +3,28 @@ package com.example.uimastyleit;
 public class Item {
     private User user;
     private String title;
+    private String condition;
     private String description;
     private String imageUrl;
     private String size;
     private int price;
 
-    public Item(User user, String title, String description, String size, int price) {
+    public Item(User user, String title, String condition, String description, String size, int price, String imageUrl) {
         this.user = user;
+        this.condition = condition;
         this.description = description;
         this.title = title;
         this.size = size;
         this.price = price;
+        this.imageUrl = imageUrl;
     }
 
     public String getTitle() { return this.title; }
     public int getPrice() { return this.price; }
     public String getSize() { return this.size; }
+    public String getCondition() { return this.condition; }
+    public String getDescription() { return this.description; }
+    public String getImageUrl() { return this.imageUrl; }
+    public User getUser() { return this.user; }
 
 }
